196f8f8a9c5424774939d90acef7ae490a7a3602
naxifa <maliha1@usf.edu>
2024-09-08 17:15:36 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,10 +1,10 @@
 
-// Task 1: Create a Function to Calculate Average Sales
+// Task 1: Creating a Function to Calculate Average Sales
 
 function calculateAverageSales(sales) 
 {
 if (sales.length === 0) 
-{return 0}; 
+{return 0;} 
 console.log(`Sales Figures: , ${sales}`); // Individual sales values
 
 const totalSales = sales.reduce((acc,sale) => acc + sale, 0); 
@@ -16,11 +16,19 @@ console.log(`Average Sales: , ${averageSales}`); // Average sales value
 return averageSales; 
 }
 
-// Example test
-let salesFigures = [6000,7500,9800];
-calculateAverageSales(salesFigures);
 
+// Task 2: Creating a Function to Determine Performance Rating
 
-
+function determinePerformanceRating(averageSales) 
+{
+    if (averageSales>10000)
+    {return "Excellent";}
+    else if (averageSales >= 7000 && averageSales <= 10000) 
+    {return "Good";}
+    else if (averageSales >= 4000 && averageSales <= 7000)
+    {return "Satisfactory";}
+    else if (averageSales<4000) 
+    {return "Needs Improvement";}
+}
 
 
